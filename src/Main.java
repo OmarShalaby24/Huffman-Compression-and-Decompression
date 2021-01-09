2b37129f86aa2adeaa2cb126320f24bf6adc4c10
Omar <ramo_24@outlook.com>
2021-01-09 08:23:54 +0200

    test commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.PriorityQueue;
 public class Main {
     public static void main(String[] args) {
         String str;
-        str = "the paragraph should be here";
+        str = "the paragraph should be here, hi";
 
         System.out.println(str.charAt(0));
 
