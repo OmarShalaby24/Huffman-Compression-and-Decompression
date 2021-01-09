f47e5f7eda53da27e63ea18b1b09fd1e30f59469
Omar <ramo_24@outlook.com>
2021-01-09 08:23:00 +0200

    test commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) {
-        String str = new String();
-        str = "my name is Omar";
+        String str;
+        str = "the paragraph should be here";
 
         System.out.println(str.charAt(0));
 
