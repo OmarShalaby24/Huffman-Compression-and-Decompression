47d51c411096482d64e8c6a818b372b7e9d522ee
Omar <ramo_24@outlook.com>
2021-01-09 08:28:48 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.PriorityQueue;
 public class Main {
     public static void main(String[] args) {
         String str;
-        str = "the paragraph should be here, hi";
+        str = "Hello World";
 
         System.out.println(str.charAt(0));
 
